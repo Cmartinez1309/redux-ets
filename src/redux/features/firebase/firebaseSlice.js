974f1c96e15c47407407f10100cd26c9da2f3a11
Cmartinez1309 <christianpat1309@gmail.com>
2022-11-22 05:02:22 -0600

    Agregar y ver listas con Firebase

--- src/redux/features/firebase/firebaseSlice.js
+++ src/redux/features/firebase/firebaseSlice.js
@@ -1,187 +1,230 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import firebaseAuth from "../../../firebase/firebase";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import firebaseAuth from '../../../firebase/firebase';
 import { PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
 
+import { getDatabase, ref, set } from 'firebase/database';
+
 const initialState = {
-    error: {
-        errorOccured: false,
-        errorMain: '',
-        errorBody: ''
-    },
-    phoneAuth: {
-        verificationId: ''
-    },
-    user: {},
-    facebook: {
-        accessToken: ''
-    }
-}
+  error: {
+    errorOccured: false,
+    errorMain: '',
+    errorBody: '',
+  },
+  phoneAuth: {
+    verificationId: '',
+  },
+  user: {},
+  facebook: {
+    accessToken: '',
+  },
+};
 
-export const signUpEmail = createAsyncThunk('firebase/signUpEmail', async ({ email, password, capRef }) => {
+export const signUpEmail = createAsyncThunk(
+  'firebase/signUpEmail',
+  async ({ email, password, capRef }) => {
     try {
-        const auth = firebaseAuth.getAuth()
-        const userCredential = await firebaseAuth.createUserWithEmailAndPassword(auth, email, password)
-        return { user: userCredential.user }
-    }
-    catch (err) {
-        return { errorCode: err.code }
-    }
-})
+      const auth = firebaseAuth.getAuth();
 
-export const signUpPhoneNumberVerify = createAsyncThunk('firebase/signUpPhoneNumberVerify', async ({ verificationId, otp }) => {
-    try {
-        const auth = firebaseAuth.getAuth()
-        const userCredential = PhoneAuthProvider.credential(verificationId, otp)
-        const response = await signInWithCredential(auth, userCredential)
-        return response
+      const userCredential = await firebaseAuth.createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const db = getDatabase();
+      set(ref(db, 'users/' + userCredential.user.uid), {
+        email: userCredential.user.email,
+        uid: userCredential.user.uid,
+      });
+
+      return { user: userCredential.user };
     } catch (err) {
-        return { errorCode: err.code }
+      return { errorCode: err.code };
     }
-})
+  }
+);
 
-export const logInPhoneNumberVerify = createAsyncThunk('firebase/logInśPhoneNumberVerify', async ({ verificationId, otp }) => {
+export const signUpPhoneNumberVerify = createAsyncThunk(
+  'firebase/signUpPhoneNumberVerify',
+  async ({ verificationId, otp }) => {
     try {
-        const auth = firebaseAuth.getAuth()
-        const userCredential = PhoneAuthProvider.credential(verificationId, otp)
-        const response = await signInWithCredential(auth, userCredential)
-        return response
+      const auth = firebaseAuth.getAuth();
+      const userCredential = PhoneAuthProvider.credential(verificationId, otp);
+      const response = await signInWithCredential(auth, userCredential);
+      return response;
     } catch (err) {
-        return { errorCode: err.code }
+      return { errorCode: err.code };
     }
-})
+  }
+);
 
-export const logInEmail = createAsyncThunk('firebase/logInEmail', async ({ email, password }) => {
+export const logInPhoneNumberVerify = createAsyncThunk(
+  'firebase/logInśPhoneNumberVerify',
+  async ({ verificationId, otp }) => {
     try {
-        console.log('--> logging in using email & password');
-        const auth = firebaseAuth.getAuth()
-        const userCredential = await firebaseAuth.signInWithEmailAndPassword(auth, email, password)
-        return { user: userCredential.user }
+      const auth = firebaseAuth.getAuth();
+      const userCredential = PhoneAuthProvider.credential(verificationId, otp);
+      const response = await signInWithCredential(auth, userCredential);
+      return response;
+    } catch (err) {
+      return { errorCode: err.code };
     }
-    catch (err) {
-        return { errorCode: err.code }
-    }
-})
+  }
+);
 
-export const signOutUser = createAsyncThunk('firebase/signOutUser', async () => {
-    const auth = firebaseAuth.getAuth()
-    await firebaseAuth.signOut(auth)
-})
+export const logInEmail = createAsyncThunk(
+  'firebase/logInEmail',
+  async ({ email, password }) => {
+    try {
+      console.log('--> logging in using email & password');
+      const auth = firebaseAuth.getAuth();
+      const userCredential = await firebaseAuth.signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return { user: userCredential.user };
+    } catch (err) {
+      return { errorCode: err.code };
+    }
+  }
+);
+
+export const signOutUser = createAsyncThunk(
+  'firebase/signOutUser',
+  async () => {
+    const auth = firebaseAuth.getAuth();
+    await firebaseAuth.signOut(auth);
+  }
+);
 
 const firebaseSlice = createSlice({
-    name: 'firebase',
-    initialState,
-    reducers: {
-        updateVerificationId: (state, action) => {
-            state.phoneAuth.verificationId = action.payload.verificationId
-        },
-        updateErrorMessage: (state) => {
-            state.error.errorBody = ''
-        }
+  name: 'firebase',
+  initialState,
+  reducers: {
+    updateVerificationId: (state, action) => {
+      state.phoneAuth.verificationId = action.payload.verificationId;
     },
-    extraReducers(builder) {
-        builder
-            .addCase(signUpEmail.fulfilled, (state, action) => {
-                console.log(' --> signUpEmail fulfilled');
-                console.log(action);
-                if (action.payload.errorCode) {
-                    switch (action.payload.errorCode) {
-                        case 'auth/email-already-in-use':
-                            state.error.errorBody = 'Correo electrónico ya en uso. Por favor use un correo electrónico differente'
-                            break;
-                    }
-                }
-                if (action.payload.user) {
-                    state.user = action.payload.user
-                }
-            })
-            .addCase(signUpEmail.rejected, () => {
-                console.log(' --> signUpEmail rejected');
-            })
-            .addCase(signUpPhoneNumberVerify.pending, () => {
-                console.log(' --> signUpEmail pending');
-            })
-            .addCase(signUpPhoneNumberVerify.fulfilled, (state, action) => {
-                console.log(' --> singupPhoneNumberVerify fulfilled.');
-                console.log(action.payload);
-                if (action.payload.errorCode) {
-                    switch (action.payload.errorCode) {
-                        case 'auth/invalid-verification-code':
-                            state.error.errorBody = 'Incorrect verification code entered.'
-                            break
-                        case 'auth/code-expired':
-                            state.error.errorBody = 'Verification Code expired. Request a new one.'
-                            break
-                    }
-                }
-                if (action.payload._tokenResponse) {
-                    switch (action.payload._tokenResponse.isNewUser) {
-                        case true: state.user = action.payload.user
-                            break;
-                        case false: state.error.errorBody = 'Phone number already is use. Please use a different phone number.'
-                            break
-                    }
-                }
-            })
-            .addCase(signUpPhoneNumberVerify.rejected, (state, action) => {
-                console.log(' --> SignedIn Unsuccessful');
-                console.log(action.payload);
-            })
-            .addCase(logInEmail.fulfilled, (state, action) => {
-                console.log(' --> logInEmail fulfilled');
-                if (action.payload.errorCode) {
-                    console.log(action.payload.errorCode);
-                    switch (action.payload.errorCode) {
-                        case 'auth/wrong-password':
-                            state.error.errorBody = 'Contraseña incorrecta. Por favor intente de nuevo.'
-                            break;
-                        case 'auth/user-not-found':
-                            state.error.errorBody = 'Correo electronico no esta registrado. Por favor intente con un correo electrónico diferente'
-                            break;
-                        case 'auth/user-disabled':
-                            state.error.errorBody = 'Su cuenta esta temporalmente deshabilitada. Intente de nuevo despues de unos minutos.'
-                            break;
-                    }
-                }
-                if (action.payload.user) {
-                    console.log(' * Signed in successful');
-                    state.user = action.payload.user
-                }
-            })
-            .addCase(logInPhoneNumberVerify.fulfilled, (state, action) => {
-                console.log(' --> logInPhoneNumberVerify fulfilled.');
-                console.log(action.payload);
-                if (action.payload.errorCode) {
-                    switch (action.payload.errorCode) {
-                        case 'auth/invalid-verification-code':
-                            state.error.errorBody = 'Incorrect verification code entered.'
-                            break
-                        case 'auth/code-expired':
-                            state.error.errorBody = 'Verification Code expired. Request a new one.'
-                            break
-                    }
-                }
-                if (action.payload._tokenResponse) {
-                    console.log(action.payload._tokenResponse);
-                    switch (action.payload._tokenResponse.isNewUser) {
-                        case true:
-                            state.error.errorBody = 'This phone number is not registered, but is now signed up with our instagram clone servers. Please login again to continue.'
-                            break;
-                        case false:
-                            state.user = action.payload.user
-                            break
-                    }
-                }
-            })
-            .addCase(signOutUser.fulfilled, (state) => {
-                console.log(' -> singOutUser fulfilled')
-                state.user = {}
-            })
-            .addCase(signOutUser.rejected, (state) => {
-                state.error.errorBody = 'Error occured. Please try again after sometime.'
-            })
-    }
-})
+    updateErrorMessage: (state) => {
+      state.error.errorBody = '';
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(signUpEmail.fulfilled, (state, action) => {
+        console.log(' --> signUpEmail fulfilled');
+        console.log(action);
+        if (action.payload.errorCode) {
+          switch (action.payload.errorCode) {
+            case 'auth/email-already-in-use':
+              state.error.errorBody =
+                'Correo electrónico ya en uso. Por favor use un correo electrónico differente';
+              break;
+          }
+        }
+        if (action.payload.user) {
+          state.user = action.payload.user;
+        }
+      })
+      .addCase(signUpEmail.rejected, () => {
+        console.log(' --> signUpEmail rejected');
+      })
+      .addCase(signUpPhoneNumberVerify.pending, () => {
+        console.log(' --> signUpEmail pending');
+      })
+      .addCase(signUpPhoneNumberVerify.fulfilled, (state, action) => {
+        console.log(' --> singupPhoneNumberVerify fulfilled.');
+        console.log(action.payload);
+        if (action.payload.errorCode) {
+          switch (action.payload.errorCode) {
+            case 'auth/invalid-verification-code':
+              state.error.errorBody = 'Incorrect verification code entered.';
+              break;
+            case 'auth/code-expired':
+              state.error.errorBody =
+                'Verification Code expired. Request a new one.';
+              break;
+          }
+        }
+        if (action.payload._tokenResponse) {
+          switch (action.payload._tokenResponse.isNewUser) {
+            case true:
+              state.user = action.payload.user;
+              break;
+            case false:
+              state.error.errorBody =
+                'Phone number already is use. Please use a different phone number.';
+              break;
+          }
+        }
+      })
+      .addCase(signUpPhoneNumberVerify.rejected, (state, action) => {
+        console.log(' --> SignedIn Unsuccessful');
+        console.log(action.payload);
+      })
+      .addCase(logInEmail.fulfilled, (state, action) => {
+        console.log(' --> logInEmail fulfilled');
+        if (action.payload.errorCode) {
+          console.log(action.payload.errorCode);
+          switch (action.payload.errorCode) {
+            case 'auth/wrong-password':
+              state.error.errorBody =
+                'Contraseña incorrecta. Por favor intente de nuevo.';
+              break;
+            case 'auth/user-not-found':
+              state.error.errorBody =
+                'Correo electronico no esta registrado. Por favor intente con un correo electrónico diferente';
+              break;
+            case 'auth/user-disabled':
+              state.error.errorBody =
+                'Su cuenta esta temporalmente deshabilitada. Intente de nuevo despues de unos minutos.';
+              break;
+          }
+        }
+        if (action.payload.user) {
+          console.log(' * Signed in successful');
+          state.user = action.payload.user;
+        }
+      })
+      .addCase(logInPhoneNumberVerify.fulfilled, (state, action) => {
+        console.log(' --> logInPhoneNumberVerify fulfilled.');
+        console.log(action.payload);
+        if (action.payload.errorCode) {
+          switch (action.payload.errorCode) {
+            case 'auth/invalid-verification-code':
+              state.error.errorBody = 'Incorrect verification code entered.';
+              break;
+            case 'auth/code-expired':
+              state.error.errorBody =
+                'Verification Code expired. Request a new one.';
+              break;
+          }
+        }
+        if (action.payload._tokenResponse) {
+          console.log(action.payload._tokenResponse);
+          switch (action.payload._tokenResponse.isNewUser) {
+            case true:
+              state.error.errorBody =
+                'This phone number is not registered, but is now signed up with our instagram clone servers. Please login again to continue.';
+              break;
+            case false:
+              state.user = action.payload.user;
+              break;
+          }
+        }
+      })
+      .addCase(signOutUser.fulfilled, (state) => {
+        console.log(' -> singOutUser fulfilled');
+        state.user = {};
+      })
+      .addCase(signOutUser.rejected, (state) => {
+        state.error.errorBody =
+          'Error occured. Please try again after sometime.';
+      });
+  },
+});
 
-export const { updateVerificationId, updateErrorMessage } = firebaseSlice.actions
+export const { updateVerificationId, updateErrorMessage } =
+  firebaseSlice.actions;
 
-export default firebaseSlice.reducer
+export default firebaseSlice.reducer;
--- src/screens/AppStack/HomeScreen.js
+++ src/screens/AppStack/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -9,6 +9,7 @@ import {
   Dimensions,
   Linking,
   TextInput,
+  FlatList,
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { signOutUser } from '../../redux/features/firebase/firebaseSlice';
@@ -17,14 +18,45 @@ import { CHANGE_LOADING } from '../../redux/features/loadingSlice';
 import AlertModal from '../../components/modals/AlertModal';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import {
+  getDatabase,
+  ref,
+  child,
+  get,
+  push,
+  set,
+  onValue,
+  orderByChild,
+  orderByValue,
+  query,
+  equalTo,
+} from 'firebase/database';
+import { async } from '@firebase/util';
+import * as firebase from 'firebase/app';
+import firebaseAuth from '../../firebase/firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { snapshotToArray } from '../../firebase/helpers';
 
 const HomeScreen = () => {
   const [message, setMessage] = useState('');
   const [isAddNewListVisible, setIsAddNewListVisible] = useState(false);
 
+  const [isAddNewList, setIsAddNewList] = useState('');
+  const [isTotalCount, setIsTotalCount] = useState(0);
+  const [isHaciendoCount, setIsHaciendoCount] = useState(0);
+  const [isHechoCount, setIsHechoCount] = useState(0);
+  const [isLista, setLista] = useState([]);
+  const [isListaHaciendo, setListaHaciendo] = useState([]);
+  const [isListaHecho, setListaHecho] = useState([]);
+  const [isCurrentUser, setIsCurrentUser] = useState('');
+
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.loading.value);
   const navigation = useNavigation();
+
+  const errorMessage = useSelector(
+    (state) => state.firebaseStore.error.errorBody
+  );
 
   const handleSignOut = () => {
     console.log('cerrando sesión...');
@@ -47,37 +79,239 @@ const HomeScreen = () => {
     setIsAddNewListVisible(false);
   };
 
+  const addNewList = async (lista) => {
+    try {
+      const db = getDatabase();
+
+      // const mostViewedPosts = query(
+      //   ref(db, `listas/${isCurrentUser}/${key}`),
+      //   orderByValue('book'),
+      //   equalTo(lista)
+      // );
+      // const snapshot = await firebase
+      //   .database()
+      //   .ref('listas')
+      //   .child(isCurrentUser)
+      //   .orderByChild('book')
+      //   .equalTo(lista)
+      //   .once('value');
+
+      // if (snapshot.exists()) {
+      //   console.log('unable to add as book already exists');
+      // } else {
+      //   console.log('fk');
+      // }
+
+      const key = push(child(ref(db), 'listas/' + isCurrentUser)).key;
+
+      console.log(key);
+
+      set(ref(db, 'listas/' + isCurrentUser + '/' + key), {
+        lista: lista,
+        read: false,
+      });
+
+      // const response = await firebase
+      //   .database()
+      //   .ref('listas')
+      //   .child(isCurrentUser.uid)
+      //   .child(key)
+      //   .set({ titulo: lista, hecho: false });
+
+      // const db = getDatabase();
+      // set(ref(db, 'users/' + userCredential.user.uid), {
+      //   email: userCredential.user.email,
+      //   uid: userCredential.user.uid,
+      // });
+
+      setIsAddNewListVisible(false);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLista((isLista) => [...isLista, lista]);
+    setListaHaciendo((isListaHaciendo) => [...isListaHaciendo, lista]);
+    setIsTotalCount(isTotalCount + 1);
+    setIsHaciendoCount(isHaciendoCount + 1);
+    console.log(isLista);
+  };
+
+  const marcarComoLeido = (listaSeleccionada, index) => {
+    let nuevaLista = isLista.filter((lista) => lista !== listaSeleccionada);
+    let nuevaListaHaciendo = isListaHaciendo.filter(
+      (lista) => lista !== listaSeleccionada
+    );
+    setLista(nuevaLista);
+    setListaHaciendo(nuevaListaHaciendo);
+    setListaHecho((isListaHecho) => [...isListaHecho, listaSeleccionada]);
+    // setIsHaciendoCount(isHaciendoCount - 1);
+    // setIsHechoCount(isHechoCount + 1);
+  };
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setIsCurrentUser(uid);
+
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+    const db = getDatabase();
+
+    // const listas = push(child(ref(db), 'listas/' + isCurrentUser));
+
+    const dbRef = ref(getDatabase());
+    let returnArr = [];
+    get(child(dbRef, `listas/${isCurrentUser}`))
+      .then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          let item = childSnapshot.val();
+          item.key = childSnapshot.key;
+          returnArr.push(item);
+        });
+        setLista(returnArr);
+        // if (snapshot.exists()) {
+        //   let item = snapshot.val();
+        //   item.key = snapshot.key;
+        //   returnArr.push(item);
+        //   console.log(snapshot.val());
+        //   console.log(snapshot.key);
+        //   // setLista(returnArr);
+        // } else {
+        //   console.log('No data available');
+        // }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // const listasArray = snapshotToArray(listas);
+
+    // setLista(listasArray);
+
+    // const dbRef = ref(getDatabase());
+    // get(child(dbRef, `listas/${isCurrentUser}`))
+    //   .then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //       console.log(snapshot.val());
+    //       returnArr.push(snapshot.val());
+    //       console.log(isLista.book);
+    //     } else {
+    //       console.log('No data available');
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     // User logged in already or has just logged in.
+    //     console.log(user.uid);
+    //   } else {
+    //     // User not logged in or has just logged out.
+    //   }
+    // });
+    // async () => {
+    //   const userCredential = await firebaseAuth.getCurrentUser();
+    //   setIsCurrentUser(userCredential.user.uid);
+    // };
+    // const userCredential = firebaseAuth.getCurrentUser();
+    // setIsCurrentUser(userCredential.user.uid);
+    // console.log(isCurrentUser);
+    // console.log(uid);
+  });
+
+  const renderLista = (item, index) => (
+    <View
+      style={{
+        height: 35,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '79%',
+      }}
+    >
+      <View
+        style={{
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ color: 'white' }}> {item.lista} </Text>
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#a5deba',
+          alignContent: 'center',
+          justifyContent: 'center',
+          width: 100,
+        }}
+        onPress={() => marcarComoLeido(item, index)}
+      >
+        <Text style={{ textAlign: 'center' }}>Mark as read</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle='light-content' hidden={false} translucent={true} />
       {isAddNewListVisible ? (
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row', height: 30 }}>
           <TextInput
+            onChangeText={(text) => setIsAddNewList(text)}
             placeholder='Insertar titulo de lista'
             placeholderTextColor='white'
+            color='white'
           />
-          <View
+          <TouchableOpacity
             style={{
               backgroundColor: '#a5deba',
               alignContent: 'center',
               justifyContent: 'center',
+              width: 25,
             }}
+            onPress={() => addNewList(isAddNewList)}
           >
-            <Ionicons name='ios-checkmark' color='white' size={15} />
-          </View>
-          <View
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <Ionicons name='ios-checkmark' color='white' size={15} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
             style={{
               backgroundColor: '#deada5',
               alignContent: 'center',
               justifyContent: 'center',
+              width: 25,
             }}
+            onPress={hideAddNewList}
           >
-            <TouchableOpacity onPress={hideAddNewList}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
               <Ionicons name='ios-close' color='white' size={15} />
-            </TouchableOpacity>
-          </View>
+            </View>
+          </TouchableOpacity>
         </View>
       ) : null}
+
+      <FlatList
+        data={isLista}
+        renderItem={({ item }, index) => renderLista(item, index)}
+        keyExtractor={(item, index) => index.toString()}
+        ListEmptyComponent={
+          <View style={{ marginTop: 50, alignItems: 'center' }}>
+            <Text style={{ color: 'white' }}>
+              No hay nada agregado a la lista...
+            </Text>
+          </View>
+        }
+      />
 
       <TouchableOpacity onPress={showAddNewList}>
         <View>
@@ -93,7 +327,7 @@ const HomeScreen = () => {
           }}
         >
           <Text style={{ color: 'white' }}>Lista</Text>
-          <Text style={{ color: 'white' }}>0</Text>
+          <Text style={{ color: 'white' }}>{isLista.length}</Text>
         </View>
         <TouchableOpacity
           style={{
@@ -105,7 +339,7 @@ const HomeScreen = () => {
           onPress={() => navigation.navigate('Haciendo')}
         >
           <Text style={{ color: 'white' }}>Haciendo</Text>
-          <Text style={{ color: 'white' }}>0</Text>
+          <Text style={{ color: 'white' }}>{isListaHaciendo.length}</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={{
@@ -117,7 +351,7 @@ const HomeScreen = () => {
           onPress={() => navigation.navigate('Hecho')}
         >
           <Text style={{ color: 'white' }}>Hecho</Text>
-          <Text style={{ color: 'white' }}>0</Text>
+          <Text style={{ color: 'white' }}>{isListaHecho.length}</Text>
         </TouchableOpacity>
       </View>
 
@@ -139,6 +373,7 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     backgroundColor: 'black',
     paddingHorizontal: 20,
+    color: 'white',
   },
   imageContainer: {
     height: '100%',
